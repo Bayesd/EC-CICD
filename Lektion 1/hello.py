836d32c1bded670e22747d909cf0671b474b402c
Leo <gbgleo@gmail.com>
2021-04-19 14:20:24 +0200

    initial commit

---
+++ Lektion 1/hello.py
@@ -0,0 +1,13 @@
+# Kontrollera version av flask:
+# py -c "import flask; print(flask.__version__)"'
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "<h1>Hello World!</h1><p>Detta är staging-</p>"
+
+app.run(debug=True)
